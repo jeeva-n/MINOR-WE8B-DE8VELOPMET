3e7848bad40ad48e716ceba7679aed74e5c42d38
jeeva-n <103167527+jeeva-n@users.noreply.github.com>
2023-02-10 23:24:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showProducts() {
+    document.getElementById("products").style.display = "flex";
+  }
+  
+  function addToCart(productId) {
+    console.log("Product ${productId} added to cart");
+  }
+  
